88251b5ef2be78fae1b6b428e82416fb1fc0e460
SpiriDs <spirids@live.de>
2019-08-22 12:16:46 +0700

    Made the challenge until silver, solution of the Cupertino Picker Data

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,14 @@
+// import 'package:flutter/material.dart';
+
+// import 'services/networking.dart';
+
+//TODO Loesung von Angela
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+//String selectedCurrency = 'USD';
+String cryptoCurrency = 'BTC';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +39,60 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+// class CoinData {
+//   CoinData(this.selectedCurrency);
+
+//   final String selectedCurrency;
+//   int amountC = 100;
+
+//   Future<dynamic> getExchangeRate() async {
+//     NetworkHelper networkHelper = NetworkHelper(
+//         'https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD');
+//     var currencyData = await networkHelper.getData();
+//     return currencyData;
+//   }
+
+//   getCoinData(currencyData) {
+//     double value = currencyData['last'];
+//     amountC = value.toInt();
+
+//     print('The amount is $amountC');
+//   }
+// }
+
+//TODO Loesung von Angela nachvollziehen warum es bei mir nicht geklappt hat!!!!!!!!!!!!!!!
+
+const bitcoinAverageURL =
+    'https://apiv2.bitcoinaverage.com/indices/global/ticker';
+
+class CoinData {
+  //CoinData(this.currency);
+
+  //String currency;
+
+  //3. Create the Asynchronous method getCoinData() that returns a Future (the price data).
+  Future getCoinData(selectedCurrency) async {
+    //4. Create a url combining the bitcoinAverageURl with the currencies we're interested, BTC to USD.
+    String requestURL = '$bitcoinAverageURL/BTC$selectedCurrency';
+
+    //5. Make a GET request to the URL and wait for the response.
+    http.Response response = await http.get(requestURL);
+
+    //6. Check that the request was successful.
+    if (response.statusCode == 200) {
+      //7. Use the 'dart:convert' package to decode the JSON data that comes back from BitcoinAverage.
+      var decodedData = jsonDecode(response.body);
+      print(decodedData);
+      //8. Get the last price of bitcoin with the key 'last'.
+      var lastPrice = decodedData['last'];
+      //9. Output the lastPrice from the method.
+
+      return lastPrice;
+    } else {
+      //10. Handle any errors that occur during the request.
+      print(response.statusCode);
+      //Optional: throw an error if our request fails.
+      throw 'Problem with the get request';
+    }
+  }
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -9,7 +9,12 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
-  String selectedCurrency = 'USD';
+  // CoinData coinData = CoinData();
+  // int amount;
+  // String selectedCurrency = 'USD';
+  // dynamic tada = coinData.getExchangeRate();
+
+  String selectedCurrency = 'AUD';
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropDownItems = [];
@@ -19,16 +24,19 @@ class _PriceScreenState extends State<PriceScreen> {
         value: currency,
       );
       dropDownItems.add(newItem);
-      print(dropDownItems);
+      //print(dropDownItems);
     }
 
     return DropdownButton<String>(
       value: selectedCurrency,
       items: dropDownItems,
       onChanged: (value) {
+        //!getData(); not here, the mistake is that the function is called before the currency changes. so you get the selected Currency but the value of the previouse one. It have to be in the setSate Methode after the value is changed
         setState(
           () {
             selectedCurrency = value;
+            getData(); //! Important is, that the function call is in the setState-function, so it will actualize after                 the selectedCurrency changes
+
 //print(value);
           },
         );
@@ -41,14 +49,21 @@ class _PriceScreenState extends State<PriceScreen> {
     for (String currency in currenciesList) {
       var newItem = Text(currency);
       pickerItems.add(newItem);
-      print(pickerItems);
+      //print(pickerItems);
+      //print(currency);
     }
 
     return CupertinoPicker(
       backgroundColor: Colors.lightBlue,
       itemExtent: 32.0,
       onSelectedItemChanged: (selectedIndex) {
-        print(selectedIndex);
+        print(currenciesList[selectedIndex]);
+        setState(() {
+          selectedCurrency = currenciesList[
+              selectedIndex]; //! Wichtig beim Cupertino PIcker ist, dass man um an den Wert zu bekommen auf die urspruengliche Liste mit dem selectedIndex auf der jeweiligen Position zurueckgreift!
+
+          getData(); //!getData() muss in der setSate() aufgerufen werden, da davor die Waehrung geandert wird!! Der Fehler war bei mir, dass ich e
+        });
       },
       children: pickerItems,
     );
@@ -65,6 +80,35 @@ class _PriceScreenState extends State<PriceScreen> {
     }
   }
 
+  //TODO Loesung von Angela untersuchen warum es bei mir nicht funktioniert hat!!!!!!!!!
+  //12. Create a variable to hold the value and use in our Text Widget. Give the variable a starting value of '?' before the data comes back from the async methods.
+  String bitcoinValueInUSD = '?';
+
+  //11. Create an async method here await the coin data from coin_data.dart
+  void getData() async {
+    try {
+      double data = await CoinData().getCoinData(selectedCurrency);
+      //13. We can't await in a setState(). So you have to separate it out into two steps.
+      setState(() {
+        bitcoinValueInUSD = data.toStringAsFixed(0);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    //14. Call getData() when the screen loads up. We can't call CoinData().getCoinData() directly here because we can't make initState() async.
+    getData();
+  }
+
+// @override
+// void initState(){
+//   super.initState();
+
+// }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -86,7 +130,10 @@ class _PriceScreenState extends State<PriceScreen> {
               child: Padding(
                 padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
                 child: Text(
-                  '1 BTC = ? USD',
+                  //'1 BTC = ? $selectedCurrency',
+                  //TODO Loesung ANgela
+                  //15. Update the Text Widget with the data in bitcoinValueInUSD.
+                  '1 BTC = $bitcoinValueInUSD $selectedCurrency',
                   textAlign: TextAlign.center,
                   style: TextStyle(
                     fontSize: 20.0,
@@ -101,7 +148,8 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: getPicker(),
+            //child: Platform.isIOS ? iOSPicker() : androidDropdown(),    ==> Version ohne getPicker Funktion
+            child: iOSPicker(),
           ),
         ],
       ),
